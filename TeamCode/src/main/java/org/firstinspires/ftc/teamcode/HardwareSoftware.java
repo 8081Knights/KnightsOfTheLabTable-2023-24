814ec2a99ff34adafc85645e2e9aae073d2e6b53
8081Knights <89168074+8081Knights@users.noreply.github.com>
2023-08-25 15:10:17 -0500

    added a hardwareMap and uncomplete prototype tele

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareSoftware.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class HardwareSoftware {
+
+    private HardwareMap hw = null;
+    DcMotorEx frontRight    = null;
+    DcMotorEx backRight     = null;
+    DcMotorEx backLeft      = null;
+    DcMotorEx frontLeft     = null;
+
+    public void init(HardwareMap ahw){
+
+        hw= ahw;
+
+        frontLeft = hw.get(DcMotorEx.class, "FLdrive");
+        frontRight = hw.get(DcMotorEx.class, "FRdrive");
+        backLeft = hw.get(DcMotorEx.class, "BLdrive");
+        backRight = hw.get(DcMotorEx.class, "BRdrive");
+
+        frontRight.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+//
+
+        frontLeft.setDirection(DcMotorEx.Direction.FORWARD);
+        frontRight.setDirection(DcMotorEx.Direction.FORWARD);
+        backLeft.setDirection(DcMotorEx.Direction.FORWARD);
+        backRight.setDirection(DcMotorEx.Direction.FORWARD);
+
+
+
+    }
+}

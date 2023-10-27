b2caf68053a4d670824ea3fc2cddacdbd23b112c
BraydenParker <89168074+8081Knights@users.noreply.github.com>
2023-10-27 13:38:01 -0700

    starting path test code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/roadRunnerPathTest.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+@Autonomous(name="RoadRunner Path Test")
+public class roadRunnerPathTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
